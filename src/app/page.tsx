102a0c1d1aa4e2d618dbbdcf8ab0018ff86ca315
adnanhussain8082 <adnanhussain8082@gmail.com>
2025-08-09 12:00:52 +0530

    01: setup

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button"
+
+const Page = () => {
+  return (
+    <div>
+      <Button>
+        click me
+      </Button>
+    </div>
+  )
+}
+
+export default Page
